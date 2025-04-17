081130d315ae0a696a20c3a83dcd0a2d19cdacba
niloferkfs <nilofer.ipcs@gmail.com>
2025-04-17 09:05:51 +0300

    added

--- HireHorizonWebApi/Domain/Services/Admin/Interfaces/IAdminServices.cs
+++ HireHorizonWebApi/Domain/Services/Admin/Interfaces/IAdminServices.cs
@@ -1,24 +1,47 @@
 ﻿using Domain.Helpers;
 using Domain.Models;
-
-using Domain.Service.Job.DTOs;
-
-
 using Domain.Service.Profile.DTOs;
-
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Domain.Service.Admin.DTOs;
+using Domain.Services.Admin.DTOs;
 
 namespace Domain.Service.Admin.Interfaces
 {
     public interface IAdminServices
     {
-       
+        public Task<List<JobSeekerDto>> GetJobSeekers();
+
+        public Task<List<JobProviderCompany>> GetCompanies();
+
+        public Task<List<Joblist>> GetAllJobs();
+
+        public Task<List<LocationDto>> GetLocations();
+
+        public Task<LocationDto> AddLocation(LocationDto location);
+
+        public void DeleteLocationById(Guid id);
+
+        public Task<PostedSkillDTO> AddSkill(PostedSkillDTO skill);
+
+        public Task<bool> DeleteSkill(Guid skillId);
+
+        public Task<CategoryDto> AddCategory(CategoryDto category);
+
+        public Task<bool> DeleteCategoryById(Guid CategoryId);
+        public Task<IndustryDto> AddIndustry(IndustryDto industry);
+        public Task<bool> DeleteIndustryById(Guid IndustryId);
+
+        public int GetJobProviderCount();
+        public int GetCompanyCount();
+        public int GetJobCount();
+        public Task<List<JobProviderCompany>> SearchCompanies(string name);
+
+
+
     }
 
 }
--- HireHorizonWebApi/Domain/Services/Authuser/AuthUserService.cs
+++ HireHorizonWebApi/Domain/Services/Authuser/AuthUserService.cs
@@ -14,6 +14,24 @@ namespace Domain.Service.Authuser
 {
 	public class AuthUserService
 	{
-		
-	}
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IAuthUserRepository _userRepository;
+
+        public AuthUserService(IHttpContextAccessor httpContextAccessor, IAuthUserRepository userRepository)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _userRepository = userRepository;
+        }
+
+        public string GetUserId()
+        {
+            var result = string.Empty;
+            if (_httpContextAccessor.HttpContext != null)
+            {
+                result = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.Sid).Value.ToString();
+            }
+            return result;
+        }
+
+    }
 }
